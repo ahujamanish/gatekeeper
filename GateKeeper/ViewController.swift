1ee80b77ef3620d821a2ae296a3f253cbab53474
Manish Ahuja <manish@fueled.com>
2018-04-24 12:52:32 +0530

    Initial Commit

---
+++ GateKeeper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GateKeeper
+//
+//  Created by Manish Ahuja on 24/04/18.
+//  Copyright © 2018 Manish Ahuja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
